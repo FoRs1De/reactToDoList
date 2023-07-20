073f3fc128c134e80c4d77bd283bfb96f25a596d
vijayavalluri <vvvijaya56@gmail.com>
2023-07-20 14:03:06 +0200

    project todo

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppVijaya.jsx'
+import App from './App.jsx'
 import './AppVijaya.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
